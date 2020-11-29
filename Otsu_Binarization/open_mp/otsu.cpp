c6601150e90b27c86dfe6db1137d4f977d8ecdde
Sbali <sbali@andrew.cmu.edu>
2020-11-29 12:08:00 -0500

    some scan implementation

--- Otsu_Binarization/open_mp/otsu.cpp
+++ Otsu_Binarization/open_mp/otsu.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <cstdlib>
 #include <limits>
+#include <math.h>
 
 //#include <random>
 #include <vector>
@@ -15,6 +16,36 @@
 #define CHANNEL_NUM 1
 #define MAX_INTENSITY 256
 #define OMP_NESTED true
+
+
+void scan_parallel(double* &cum_histogram, double* &cum_sum){
+    int i, d, k;
+
+    // up sweep
+    for(d = 0; d<num_levels; d++){
+        #pragma omp parallel for shared(cum_histogram, cum_sum)
+        for(k=0; k<MAX_INTENSITY/1<<(d+1); k++){
+            cum_histogram[k + pow(2, d+1) -1] = cum_histogram[k+pow(2, d) -1] + cum_histogram[k+pow(2, d+1)-1];
+            cum_sum[k + 1<<(d+1) -1] = cum_sum[k+(1<<d) -1] + cum_sum[k+ 1<<(d+1)-1];
+        }
+    }
+
+    // down sweep 
+    double tmp_sum, tmp_histogram;
+    a[MAX_INTENSITY-1] = 0;
+    for (d = num_levels-1; d=0; d--) {
+        #pragma omp parallel for shared(cum_histogram, cum_sum)
+        for (k = 0; i < MAX_INTENSITY/1<<(d+1); i++){
+            tmp_histogram = cum_histogram[k + 1<<d];
+            tmp_sum = cum_sum[k + 1<<d];
+            cum_histogram[k + 1<<d -1] = cum_histogram[k+1<<(d+1) -1];
+            cum_histogram[k + 1<<(d+1) -1] = tmp_histogram + cum_histogram[k+1<<(d+1) -1];
+            cum_sum[k + 1<<d -1] = cum_sum[k+1<<(d+1) -1];
+            cum_sum[k + 1<<(d+1) -1] = tmp_sum + cum_sum[k+1<<(d+1) -1];
+        }
+    }
+
+}
 
 
 int otsu_binarization(uint8_t* &gray_img, int width, int height){
@@ -29,48 +60,41 @@ int otsu_binarization(uint8_t* &gray_img, int width, int height){
     double total_sum=0.0;
     double hist_sum=0.0;
     double hist_val_sum=0.0;
-
+    int i=0;
     double p1_sum, p2_sum, p1_num, p2_num = 0;
     double p1_mu, p2_mu, var, threshold = 0;
     int total_pts = width * height;
     double max_var = -1;
     double mu_diff;
+    int total_num = 0;
+    int thresh = 0;
     uint8_t *new_img = (uint8_t *) malloc(total_pts * sizeof(uint8_t));
 
-    #pragma omp parallel for shared(histogram)
-    for(int i=0; i< MAX_INTENSITY; i++){
+    #pragma omp parallel for shared(histogram) private(i)
+    for(i=0; i< MAX_INTENSITY; i++){
         histogram[i] = 0.0;
     }
   
     //TEST ??
-    #pragma omp parallel for shared(histogram, gray_img) 
-    for (int i=0; i<width* height; i++) {
+    #pragma omp parallel for shared(histogram, gray_img)
+    for (i=0; i<width* height; i++) {
         #pragma omp atomic
         histogram[gray_img[i]] += 1.0;
     }
     
     #pragma omp parallel for shared(histogram) reduction(+: total_sum)
-    for(int i=0; i<MAX_INTENSITY; i++){
+    for(i=0; i<MAX_INTENSITY; i++){
         total_sum += double(i) * (histogram[i]) ;
+        // set initial values for the reduction
+        cum_histogram[i] = histogram[i];
+        cum_sum[i] = i * histogram[i];
     }
 
-    #pragma omp simd reduction(inscan, +:hist_sum) 
-    for(int i=0; i<MAX_INTENSITY; i++){
-        hist_sum += histogram[i];
-        #pragma omp scan inclusive(hist_sum)
-        cum_histogram[i] = hist_sum;
-    }
+    int num_levels = ceil(log(MAX_INTENSITY)/log(2));
+    scan_parallel(cum_histogram, cum_sum);
 
-    //#pragma omp simd reduction(inscan, +:hist_val_sum) 
-    for(int i=0; i<MAX_INTENSITY; i++){
-        hist_val_sum += i * histogram[i];
-        //#pragma omp scan inclusive(hist_val_sum)
-        cum_sum[i] = hist_val_sum;
-    }
-
-    #pragma omp parallel for private(p1_num, p2_num, p1_sum, p2_sum, p1_mu, p2_mu, var, mu_diff) shared(histogram, threshold, total_pts, total_sum) 
-    for(int thresh=0; thresh<MAX_INTENSITY; thresh++){
-        //printf("%d\n", histogram[thresh]);
+    #pragma omp parallel for private(p1_num, p2_num, p1_sum, p2_sum, p1_mu, p2_mu, var, mu_diff) shared(cum_histogram, threshold, total_pts, total_sum) 
+    for(thresh=0; thresh<MAX_INTENSITY; thresh++){
         p1_num = cum_histogram[thresh];
         p2_num = total_pts - p1_num;
         if(p1_num == 0 || p2_num==0){
@@ -84,14 +108,13 @@ int otsu_binarization(uint8_t* &gray_img, int width, int height){
         var = p1_num * p2_num * mu_diff * mu_diff;
         #pragma omp critical
         if(var > max_var){
-            //printf("SET %f %f \n", threshold, max_var);
             threshold = thresh;
             max_var = var;
         }
     }
 
     #pragma omp parallel for shared(new_img, threshold)
-    for(int i=0; i< total_pts; i++){
+    for(i=0; i< total_pts; i++){
         if(gray_img[i]> threshold){
             new_img[i] = 255;
         }
