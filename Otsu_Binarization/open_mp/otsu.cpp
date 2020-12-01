bbfc0a7d057fb272035e2e4e65d3dd9ffcc79cf8
kmeshx <32246476+kmeshx@users.noreply.github.com>
2020-12-01 13:07:52 -0500

    Otsu parallel with speedup
    checkpoint

--- Otsu_Binarization/open_mp/otsu.cpp
+++ Otsu_Binarization/open_mp/otsu.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <algorithm>
 //#include "cycletimer.h"
 #include <omp.h>
 #include <unistd.h>
@@ -6,7 +7,7 @@
 #include <cstdlib>
 #include <limits>
 #include <math.h>
-
+#include "cycletimer.h"
 //#include <random>
 #include <vector>
 #define STB_IMAGE_IMPLEMENTATION
@@ -16,31 +17,31 @@
 #define CHANNEL_NUM 1
 #define MAX_INTENSITY 256
 #define OMP_NESTED true
-
+#define NT 24
 
 void scan_parallel(double* &cum_histogram, double* &cum_sum){
     int i, d, k;
-    for(i=0; i<5; i++)
+    /*for(i=0; i<5; i++)
     {
         std::cout<< cum_histogram[i]<<" ";
     }
-    std::cout<<"\n";
+    std::cout<<"\n";*/
     for(d = 1; d<MAX_INTENSITY; d<<=1){
 
-        #pragma omp parallel for shared(cum_histogram, cum_sum)
+        #pragma omp parallel for num_threads(NT) shared(cum_histogram, cum_sum)
         for(k=0; k<MAX_INTENSITY; k+=2*d){
             cum_histogram[2*d+k-1] = cum_histogram[2*d+k-1] + cum_histogram[d+k-1] ;
             cum_sum[2*d+k-1] = cum_sum[2*d+k-1] + cum_sum[d+k-1];
         }
     }
-    
-    // down sweep 
+
+    // down sweep
     double tmp_sum, tmp_histogram;
     return;
     cum_histogram[MAX_INTENSITY-1] = 0;
     cum_sum[MAX_INTENSITY-1] = 0;
     for (d = MAX_INTENSITY/2; d>0; d>>=1) {
-        #pragma omp parallel for shared(cum_histogram, cum_sum)
+        #pragma omp parallel for num_threads(NT) shared(cum_histogram, cum_sum)
         for (k = 0; k < MAX_INTENSITY; k += 2*d){
             tmp_histogram = cum_histogram[d+k-1];
             tmp_sum = cum_sum[d+k-1];
@@ -50,12 +51,12 @@ void scan_parallel(double* &cum_histogram, double* &cum_sum){
             cum_sum[2*d+k-1] = tmp_sum + cum_sum[2*d + k-1];
         }
     }
-    for(i=0; i<5; i++)
+    /*for(i=0; i<5; i++)
     {
         std::cout<<cum_histogram[i]<<" ";
     }
     std::cout<<"\n";
-    
+    */
 
 
 }
@@ -66,8 +67,8 @@ int otsu_binarization(uint8_t* &gray_img, int width, int height){
     double histogram[MAX_INTENSITY];
     double* cum_histogram = (double*)malloc(sizeof(double) * MAX_INTENSITY);
     double* cum_sum = (double*)malloc(sizeof(double) * MAX_INTENSITY);
+    double* max_vars = (double*)malloc(sizeof(double)*MAX_INTENSITY);
 
-    
     //vector<int> histogram(MAX_INTENSITY); //gray scale image
     int index;
     double total_sum=0.0;
@@ -83,19 +84,19 @@ int otsu_binarization(uint8_t* &gray_img, int width, int height){
     int thresh = 0;
     uint8_t *new_img = (uint8_t *) malloc(total_pts * sizeof(uint8_t));
 
-    #pragma omp parallel for shared(histogram) private(i)
+    #pragma omp parallel for num_threads(NT) shared(histogram) private(i)
     for(i=0; i< MAX_INTENSITY; i++){
         histogram[i] = 0.0;
     }
-  
+
     //TEST ??
-    #pragma omp parallel for shared(histogram, gray_img)
+    //#pragma omp parallel for num_threads(NT) shared(histogram, gray_img)
     for (i=0; i<width* height; i++) {
-        #pragma omp atomic
+        //#pragma omp atomic
         histogram[gray_img[i]] += 1.0;
     }
-    
-    #pragma omp parallel for shared(histogram) reduction(+: total_sum)
+
+    #pragma omp parallel for num_threads(NT) shared(histogram) reduction(+: total_sum)
     for(i=0; i<MAX_INTENSITY; i++){
         total_sum += double(i) * (histogram[i]) ;
         // set initial values for the reduction
@@ -105,7 +106,7 @@ int otsu_binarization(uint8_t* &gray_img, int width, int height){
 
     scan_parallel(cum_histogram, cum_sum);
 
-    #pragma omp parallel for private(p1_num, p2_num, p1_sum, p2_sum, p1_mu, p2_mu, var, mu_diff) shared(cum_histogram, threshold, total_pts, total_sum) 
+    #pragma omp parallel for num_threads(NT) private(p1_num, p2_num, p1_sum, p2_sum, p1_mu, p2_mu, var, mu_diff) shared(cum_histogram, threshold, total_pts, total_sum)
     for(thresh=0; thresh<MAX_INTENSITY; thresh++){
         p1_num = cum_histogram[thresh];
         p2_num = total_pts - p1_num;
@@ -118,14 +119,24 @@ int otsu_binarization(uint8_t* &gray_img, int width, int height){
         p2_mu = p2_sum/p2_num;
         mu_diff = (p1_mu - p2_mu);
         var = p1_num * p2_num * mu_diff * mu_diff;
-        #pragma omp critical
+        /*#pragma omp critical
         if(var > max_var){
             threshold = thresh;
             max_var = var;
-        }
+        }*/
+	max_vars[thresh] = var;
     }
 
-    #pragma omp parallel for shared(new_img, threshold)
+    #pragma omp parallel for num_threads(NT) shared(max_vars) reduction(max: max_var)
+    for(thresh=0; thresh<MAX_INTENSITY; thresh++){
+	max_var = std::max(max_var, max_vars[thresh]);
+	}
+    #pragma omp parallel for num_threads(NT) shared(max_vars)
+    for(thresh=0; thresh<MAX_INTENSITY; thresh++){
+	if(max_vars[thresh]==max_var) threshold=thresh;
+}
+
+    #pragma omp parallel for num_threads(NT) shared(new_img, threshold)
     for(i=0; i< total_pts; i++){
         if(gray_img[i]> threshold){
             new_img[i] = 255;
@@ -134,8 +145,8 @@ int otsu_binarization(uint8_t* &gray_img, int width, int height){
             new_img[i] = 0;
         }
     }
-  
-  stbi_write_png("cs_test1_out.png", width, height, CHANNEL_NUM, new_img, width*CHANNEL_NUM);  
+
+  stbi_write_png("cs_test1_out.png", width, height, CHANNEL_NUM, new_img, width*CHANNEL_NUM);
   printf("Finished otsu\n");
   return 1;
 }
@@ -143,10 +154,14 @@ int otsu_binarization(uint8_t* &gray_img, int width, int height){
 
 int main(int argc, char **argv){
     printf("Starting off ... \n");
-    const char *img_file = "cs_test1.jpg";
+    const char *img_file = "is_test1.jpg";
     int width, height, bpp;
-    uint8_t* gray_img = stbi_load(img_file, &width, &height, &bpp, CHANNEL_NUM);  
+    uint8_t* gray_img = stbi_load(img_file, &width, &height, &bpp, CHANNEL_NUM);
+    omp_set_num_threads(NT);
+    double start_time_exc = currentSeconds();
     otsu_binarization(gray_img, width, height);
+    double end_time = currentSeconds();
+    double duration_exc = end_time - start_time_exc;
+    fprintf(stdout, "Time: %f\n", duration_exc);
     return 1;
 }
-
