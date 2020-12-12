997494ea46024e0cb007bca0654b4cd1de9b2a87
Sbali <sbali@andrew.cmu.edu>
2020-12-12 08:21:17 -0500

    cuda shared mem

--- EdgeDetection/cuda/edge.cu
+++ EdgeDetection/cuda/edge.cu
@@ -1,6 +1,12 @@
 /*
-    opts: ./edge <img_path> 
+    opts: ./edge /afs/andrew.cmu.edu/usr12/sbali/private/proj/images/building.jpg 32
 */
+/* 
+    analyze on both shared, not shared
+    add timing without memory copying 
+    on different block sizes
+*/
+
 #include <unistd.h>
 #include <algorithm>
 #include <cstdlib>
@@ -9,73 +15,134 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <driver_functions.h>
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "../../utils/stb_image.h"
-
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../../utils/stb_image_write.h"
 #include "../../utils/cycletimer.h"
 #define CHANNEL_NUM 1
+#define THREADS_PER_BLK 32
 
-float HSOBEL_H[3] = {-1.0/3, 0, 1.0/3};
-float HSOBEL_V[3] = {1.0/3, 2.0/3, 1.0/3};
-float HSOBEL[9] = {1.0/3, 1.0/3, 1.0/3, 1.0/3, 1.0/3, 1.0/3, 1.0/3, 1.0/3, 1.0/3};
-//{-1.0/9, -2.0/9, -1.0/9, 0, 0, 0, 1.0/9, 2.0/9, 1.0/9};
+float HSOBEL_H[3] = {-1.0, 0, 1.0};
+float HSOBEL_V[3] = {1.0, 2.0, 1.0};
+__device__ __constant__ float HSOBEL[9] = {-1.0, -2.0, -1.0, 0, 0, 0, 1.0, 2.0, 1.0};
 
 
-__global__ void single_kernel(uint8_t* old_img, uint8_t* new_img, float* kernel, int k_width, int k_height, int img_width, int img_height){
+__global__ void single_kernel(uint8_t* old_img, uint8_t* new_img, float kernel[9], int k_width, int k_height, int img_width, int img_height){
     int i, j, jj, ii;
     i = blockIdx.x * blockDim.x + threadIdx.x;
     j = blockIdx.y * blockDim.y + threadIdx.y;
-    if(i<img_width && j<img_height){
-
-
+    if(i>=img_width || j>= img_height) return;
     float tmp=0.f;
-    //int jj_last = min(j + k_height, img_height);
-    //int ii_last = min(i + k_width, img_width);
-    for(jj = j; jj<j + k_height; jj++){
-        for(ii = i; ii< i + k_width; ii++){
-            float og_tmp  = tmp;
-            if(jj<img_height && ii<img_width){
-            tmp += kernel[(jj-j) * k_width + (ii-i)] * (float)(old_img[jj * img_width + ii]);
-            }
+    int jj_last = min(j+k_height, img_height);
+    int ii_last = min(i+k_width, img_width);
+
+    for(jj = j; jj< jj_last; jj++){
+        for(ii = i; ii< ii_last; ii++){            
+            tmp += HSOBEL[(jj-j) * k_width + (ii-i)] * (old_img[jj * img_width + ii]);
         }
     }
-    new_img[j*img_width + i] = (uint8_t)(tmp);
-    }
+    new_img[j*img_width + i] = (uint8_t)sqrt(tmp*tmp);
+
 }
 
+//shared requires constant
+//assumes k_width, k_height = 3
+__global__ void shared_kernel(uint8_t* old_img, uint8_t* new_img, int img_width, int img_height){
+    int i, j, jj, ii, support_id, img_id, n_img_id;
+    i = blockIdx.x * blockDim.x + threadIdx.x;
+    j = blockIdx.y * blockDim.y + threadIdx.y;
+    
+    __shared__ float support[(THREADS_PER_BLK+2)*(THREADS_PER_BLK+2)]; 
+    if(i<img_width && j< img_height) {
+        support_id = (THREADS_PER_BLK+2)*threadIdx.y + threadIdx.x;
+        img_id = img_width * j + i;
+        support[support_id] =  old_img[img_id];
+
+        if(threadIdx.x < 2){
+            n_img_id = j*img_width + (THREADS_PER_BLK + i);
+            if(n_img_id<img_width * img_height)
+            support[(THREADS_PER_BLK+2)*threadIdx.y + (THREADS_PER_BLK + threadIdx.x)] = old_img[n_img_id];        
+        }
+
+        if(threadIdx.y < 2){
+            n_img_id = (THREADS_PER_BLK + j)*img_width + i;
+            if(n_img_id<img_width * img_height)
+            support[(THREADS_PER_BLK+2)*(THREADS_PER_BLK + threadIdx.y) + (threadIdx.x)] = old_img[n_img_id];
+        }
+    
+        if(threadIdx.x < 2 && threadIdx.y<2){
+            n_img_id = (THREADS_PER_BLK + j)*img_width + (THREADS_PER_BLK + i);
+            if(n_img_id<img_width * img_height)
+            support[(THREADS_PER_BLK+2)*(THREADS_PER_BLK + threadIdx.y) + (THREADS_PER_BLK + threadIdx.x)] = old_img[n_img_id];
+        }
+    }   
+    
+    __syncthreads();
+    
+    if(i<img_width && j< img_height) {
+        float tmp=0.f;
+        int jj_last = min(j+3, img_height) - j;
+        int ii_last = min(i+3, img_width) - i;
+        for(jj = 0; jj< jj_last; jj++){
+            for(ii = 0; ii< ii_last; ii++){            
+                tmp += HSOBEL[3*jj + ii] * support[(threadIdx.y+jj)*(THREADS_PER_BLK+2) + (threadIdx.x+ii)];
+            }   
+        }
+        new_img[img_id] = (uint8_t)sqrt(tmp*tmp);
+    }
 
 
-void edge_detect(uint8_t* old_img, int width, int height, int block_side) {
-    uint8_t* old_img_device;
-    uint8_t* new_img_device;
-    uint8_t* new_img = (uint8_t*)malloc(sizeof(uint8_t) * height * width * CHANNEL_NUM);
-    float* kernel_device;
-    cudaMalloc(&new_img_device, sizeof(uint8_t) * height * width*CHANNEL_NUM );
-    cudaMalloc(&old_img_device, sizeof(uint8_t) * height * width*CHANNEL_NUM );
-    cudaMalloc(&kernel_device, sizeof(float) * 9);
+}
+
+void edge_detect_single(uint8_t* &old_img, int width, int height, int block_side, uint8_t* new_img, uint8_t* new_img_device, uint8_t* old_img_device) {
     cudaMemcpy(old_img_device, old_img, sizeof(uint8_t) * height * width*CHANNEL_NUM, cudaMemcpyHostToDevice);
-    cudaMemcpy(kernel_device, HSOBEL, sizeof(float) * 9, cudaMemcpyHostToDevice);
     dim3 threadsPerBlock(block_side, block_side);
     dim3 gridDim((width+threadsPerBlock.x-1)/threadsPerBlock.x, (height+threadsPerBlock.y-1)/threadsPerBlock.y);
-    printf("HERE %d, %d; %d, %d\n", width, height, gridDim.x, gridDim.y);
-    single_kernel<<<gridDim, threadsPerBlock>>>(old_img_device, new_img_device, kernel_device, 3, 3, width, height);
+    single_kernel<<<gridDim, threadsPerBlock>>>(old_img_device, new_img_device, HSOBEL, 3, 3, width, height);
     cudaMemcpy(new_img, new_img_device, sizeof(uint8_t) * height * width * CHANNEL_NUM, cudaMemcpyDeviceToHost);
     stbi_write_png("cs_test1_out.png", width, height, CHANNEL_NUM, new_img, width*CHANNEL_NUM);  
+
 }
 
+void edge_detect_shared(uint8_t* &old_img, int width, int height, int block_side, uint8_t* new_img, uint8_t* new_img_device, uint8_t* old_img_device) {
+    cudaMemcpy(old_img_device, old_img, sizeof(uint8_t) * height * width*CHANNEL_NUM, cudaMemcpyHostToDevice);
+    dim3 threadsPerBlock(THREADS_PER_BLK, THREADS_PER_BLK);
+    dim3 gridDim((width+threadsPerBlock.x-1)/threadsPerBlock.x, (height+threadsPerBlock.y-1)/threadsPerBlock.y);
+    shared_kernel<<<gridDim, threadsPerBlock>>>(old_img_device, new_img_device, width, height);    
+    cudaMemcpy(new_img, new_img_device, sizeof(uint8_t) * height * width * CHANNEL_NUM, cudaMemcpyDeviceToHost);
+    stbi_write_png("cs_test1_out.png", width, height, CHANNEL_NUM, new_img, width*CHANNEL_NUM);  
 
-// eg: ./edge /afs/andrew.cmu.edu/usr12/sbali/private/proj/images/building.jpg 32
+}
+
+// eg: ./edge /afs/andrew.cmu.edu/usr12/sbali/private/proj/images/building.jpg 32 <type>
+// NOTE shared doesn't support arg block size it is just a place holder here
 int main(int argc, char **argv){
-    printf("Starting off ... \n");
     const char *img_file = argv[1];
     int block_side = atoi(argv[2]);
-    printf("Starting off ... %s \n", img_file);
     int width, height, bpp;
+    uint8_t* old_img_device;
+    uint8_t* new_img_device;
     uint8_t* old_img = stbi_load(img_file, &width, &height, &bpp, CHANNEL_NUM);  
-    edge_detect(old_img, width, height, block_side);
+
+    uint8_t* new_img = (uint8_t*)malloc(sizeof(uint8_t) * height * width * CHANNEL_NUM);
+    cudaMalloc(&new_img_device, sizeof(uint8_t) * height * width*CHANNEL_NUM );
+    cudaMalloc(&old_img_device, sizeof(uint8_t) * height * width*CHANNEL_NUM );
+
+    //cudaMalloc(&kernel_device, sizeof(float) * 9);
+    //cudaMemcpy(kernel_device, HSOBEL, sizeof(float) * 9, cudaMemcpyHostToDevice);
+    char type = argv[3][0];
+    double start_time_exc = currentSeconds();
+    for(int i=0; i<200; i++){
+        if(type=='n')
+            edge_detect_single(old_img, width, height, block_side, new_img, new_img_device, old_img_device);
+        else if(type=='s')
+            edge_detect_shared(old_img, width, height, block_side, new_img, new_img_device, old_img_device);
+
+    }
+    double end_time = currentSeconds();
+    double duration_exc = end_time - start_time_exc;
+    fprintf(stdout, "Time Without Startup: %f\n", duration_exc);
     return 1;
     
 }
